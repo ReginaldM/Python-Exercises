4833c278782842f5f2c9ee6e258ac98bf4d2c9cc
Reginald T. Moliki <trmoliki@outlook.com>
2019-06-01 16:53:39 +0200

    Refined Character Input exercise

--- Character Input.py
+++ Character Input.py
@@ -2,30 +2,9 @@ import datetime
 
 currentDate = datetime.date.today().strftime("%Y")
 
+userName = input("Enter your Name: ")
+userAge = int(input('Please enter your age: '))
 
-print(currentDate)
+predictYear = 100 - userAge
 
-
-
-
-
-##print(currentDate.strftime(' %A %b %d, %Y'))
-
-userInput = input('Please enter your next Birthdate (DD/MM/YYYY): ')
-
-userDoB = datetime.datetime.strptime(userInput,'%d/%m/%Y').date()
-
-##userYear = datetime.datetime.strptime(userInput,'%d/%m/%Y').year()
-
-print(userDoB)
-
-## nextBirthDate = \
-##              datetime.datetime.strptime('30/01/2020','%d/%m/%Y').date()
-
-##differenceBwtn = userDoB - currentDate
-
-##print(differenceBwtn)
-
-##testeddate = '4/25/2015'
-##dt_obj = datetime.datetime.strptime(testeddate,'%m/%d/%Y')
-##print(dt_obj)
+print(f"Did you know {userName}, that you will turn a 100 years old in the year {int(currentDate) + predictYear}")
