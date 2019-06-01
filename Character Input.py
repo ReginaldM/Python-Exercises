2dfe3c2d26472fce4e325efa673c90a54788336f
Reginald T. Moliki <trmoliki@outlook.com>
2019-06-01 17:04:51 +0200

    Refined character input

--- Character Input.py
+++ Character Input.py
@@ -4,7 +4,10 @@ currentDate = datetime.date.today().strftime("%Y")
 
 userName = input("Enter your Name: ")
 userAge = int(input('Please enter your age: '))
+numTimes = int(input("Enter any number: "))
 
 predictYear = 100 - userAge
 
-print(f"Did you know {userName}, that you will turn a 100 years old in the year {int(currentDate) + predictYear}")
+
+print(numTimes*f"\nDid you know {userName}, that you will turn a 100 years old in the year {int(currentDate) + predictYear}")
+
